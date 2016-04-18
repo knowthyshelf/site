2bda54ea389bb1f046f5d3a9a72d2c0971fc8bb0
Michael Lobel <mglobel@gmail.com>
2016-04-18 17:57:13 -0500

    home page content

--- app/components/Bookshelf.jsx
+++ app/components/Bookshelf.jsx
@@ -8,7 +8,7 @@ var searchBoxStyles = {
 }
 
 var bookTemplate = `
-<div style='background: url({{imageUrl}}); height: 300px; width: 200px; margin: 3px;'>
+<div style='background-color: gray; height: 300px; width: 200px; margin: 8px;'>
   <div style='height: 100%; width: 100%; text-align: center; display: flex; flex-direction: column; justify-content: center; font-family: sans-serif;'>
     <a href="/#/book/{{permalink}}" style=' font-size: 20px;'>{{Original Title}}</a><br /> by {{{Author}}}
   </div>
@@ -48,10 +48,9 @@ class Bookshelf extends React.Component {
 
   render() {
     return(
-      <div>
+      <div >
         <div id="search-box" style={searchBoxStyles}></div>
         <div id="hits-container"></div>
-        <a href="https://www.algolia.com/" style={searchBoxStyles}>Powered by Algolia</a>
       </div>
     );
   }
--- app/views/Home.jsx
+++ app/views/Home.jsx
@@ -3,9 +3,30 @@ import Bookshelf from '../components/Bookshelf.jsx';
 
 class Home extends React.Component{
   render() {
+    let bodyStyles = {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }
+    
+    let heroStyles = {
+      backgroundColor: 'lightgray',
+      width: '100%',
+      textAlign: 'center',
+    }
+    
     return(
-      <div>
-        <h1>Let's do this</h1>
+      <div ref='body' style={bodyStyles}>
+        <div ref='hero' style={heroStyles}>
+          {/* on scroll down, have hero h1 snap up to logo spot */}
+          <h1>KnowThyShelf</h1>
+          <h2>Discover the story behind your favorite story.</h2>
+          <h3>
+            Identification points and publishing histories for rare and 
+            interesting books.
+          </h3>
+        </div>
+        
         <Bookshelf />
       </div>
     )
