41c859ffe2dce95fa0b71cc30774cf836a9d8900
Michael Lobel <mglobel@gmail.com>
2016-06-05 00:55:39 -0500

    mobile friendly hero font sizing

--- app/views/Home.jsx
+++ app/views/Home.jsx
@@ -8,8 +8,8 @@ class Home extends React.Component{
       <div>
         <div className='hero'>
           {/* on scroll down, have hero h1 snap up to logo spot */}
-          <h1>Discover the story behind</h1>
-          <h1>your favorite story.</h1>
+          <h1>Discover the story</h1>
+          <h1>behind your favorite story.</h1>
           <h3>
             Identification points and publishing histories for rare and 
             interesting books.
