81db6235f9250285c730263b2442e659f405f1f5
Michael Lobel <mglobel@gmail.com>
2016-06-29 12:53:38 -0500

    refactor web pack.config.js and split vendor config into parts.js
    based on update from survivejs-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,15 @@
-const ghpages = require('gh-pages');
 const path = require('path');
-const merge = require('webpack-merge');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
 
-const pkg = require('./package.json');
+const parts = require('./libs/parts');
 
-const TARGET= process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build'),
-  style: path.join(__dirname, 'app/styles/main.scss')
+  style: path.join(__dirname, 'app/styles/main.scss'),
+  build: path.join(__dirname, 'build')
 };
-
-process.env.BABEL_ENV = TARGET;
 
 const common = {
   entry: {
@@ -36,7 +30,12 @@ const common = {
         test: /\.jsx?$/,
         loaders: ['babel?cacheDirectory'],
         include: PATHS.app
-     }
+     },       
+     {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
     ]
   },
   plugins: [
@@ -49,82 +48,53 @@ const common = {
   ]
 };
 
-// needed for deploy..
+var config;
 
-// Default configuration
-if(TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {
-    devtool: 'eval-source-map',
-    devServer: {
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      progress: true,
-      stats: 'errors-only',
-      host: process.env.HOST,
-      port: process.env.PORT
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loaders: ['style', 'css'],
-          include: PATHS.app
-        },
-        {
-          test: /\.scss$/,
-          loaders: ['style', 'css', 'sass'],
-          include: PATHS.style
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+  case 'stats':
+    config = merge(
+      common,
+      {
+        devtool: 'source-map',
+        output: {
+          filename: '[name].[chunkhash].js',
+          chunkFilename: '[chunkhash].js'
         }
-      ]
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-    ]
-  });
+      },
+      parts.clean(PATHS.build),
+      parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'
+      ),
+      parts.extractBundle({
+        name: 'vendor',
+        entries: ['react']
+      }),
+      parts.minify(),
+      parts.extractCSS(PATHS.style),
+      parts.purifyCSS([PATHS.app])
+    );
+    break;
+  default:
+    config = merge(
+      common,
+      {
+        devtool: 'eval-source-map'
+      },
+      parts.setupCSS(PATHS.style),
+      parts.devServer({
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        stats: 'errors-only',
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
 
-if(TARGET === 'build' || TARGET === 'stats') {
-  module.exports = merge(common, {
-    entry: {
-      vendor: Object.keys(pkg.dependencies).filter(function(v) {
-        return v !== 'alt-utils';
-      })
-    },
-    output: {
-      path: PATHS.build,
-      filename: '[name].[chunkhash].js',
-      chunkFilename: '[chunkhash].js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loader: ExtractTextPlugin.extract('style', 'css'),
-          include: PATHS.app
-        },
-        {
-          test: /\.scss$/,
-          loaders: ['style', 'css', 'sass'],
-          include: PATHS.style
-        }
-      ]
-    },
-    plugins: [
-      new CleanPlugin([PATHS.build]),
-      // new CleanPlugin([path.join(PATHS.build, '/*')],
-      new webpack.optimize.CommonsChunkPlugin({
-        names: ['vendor', 'manifest']
-      }),
-      new ExtractTextPlugin('[name].[chunkhash].css'),
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV':'"production"'
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      })
-    ]
-  });
-}
+module.exports = validate(config, {
+  quite: true
+});
