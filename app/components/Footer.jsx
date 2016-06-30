af4af0b120b499736a39a7c45dbd659e6d16ba74
Michael Lobel <mglobel@gmail.com>
2016-06-30 18:38:41 -0500

    footer component

---
+++ app/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <footer>
+        <p>
+          Disclaimer: Though we try our best to ensure accuracy, we cannot 
+          guarantee that the information listed here is 100% correct.
+        </p>
+        <br />
+        <a href='about'>About</a> | <a href='about'>F.A.Q.</a> | <a href='about'>Glossary</a> | <a href='about'>Contact</a>
+        <br />
+        <br />
+        &copy; { new Date().getFullYear() } KnowThyShelf
+      </footer>
+    )
+  }
+}
+
+export default Footer;
--- app/views/Home.jsx
+++ app/views/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Nav from '../components/Nav.jsx';
+import Footer from '../components/Footer.jsx';
 import Bookshelf from '../components/Bookshelf.jsx';
 
-class Home extends React.Component {
+class Home extends React.Component  {
   render() {
     return(
       <div id='container'>
         <Nav />
         <Bookshelf />
+        <Footer />
       </div>
     )
   }
