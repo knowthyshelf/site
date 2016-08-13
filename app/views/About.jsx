d65e378c2fbe340458bcbb5a4e901d28df8a542f
Michael Lobel <mglobel@gmail.com>
2016-08-12 19:45:13 -0500

    add contentful package for content management

--- app/views/About.jsx
+++ app/views/About.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React from 'react'
+import contentful from 'contentful'
 import styles from '../styles/about.css'
 
 class About extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      content: ''
+    }
+  }
+
+  componentDidMount() {
+    let self = this
+    let client = contentful.createClient({
+      space: 'y1iouddx4o4w',
+      accessToken: '74f3c6a99d0f8654bdf9c235f75bfe6aca3d30fd18be14072f0f030af322f1a8'
+    })
+    client.getEntry('57iV45OACs6S8yWQK2y2Yy')
+    .then(function(entry) {
+      self.setState({
+        content: entry.fields.content
+      })
+    })
+  }
+
   render() {
-    
     return(
       <div id='about' className='container'>
         <div className='content'>
           <h1>About KnowThyShelf</h1>
           <p className='description'>
-            KnowThyShelf was created in July 2015 by owner/operator Joe Sipkins. Joe is an amateur book collector who was dismayed at the lack of information readily available about his favorite titles, so he decided to do something about it. Despite the intimidating nature of the task (cataloguing hundreds or thousands or millions of books), Joe is excited to plunge into the work necessary to make the website grow. For a bibliomaniac, there could be no greater task.
+            { this.state.content }
           </p>
         </div>
       </div>
