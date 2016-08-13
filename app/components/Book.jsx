28408cdee6518af420a74f19dba0dc38d124fb3e
Michael Lobel <mglobel@gmail.com>
2016-08-12 20:35:29 -0500

    hacks to get book show view working

--- app/components/Book.jsx
+++ app/components/Book.jsx
@@ -1,49 +1,142 @@
 import React from 'react';
 import BookBox from './BookBox.jsx'
 import algoliasearch from 'algoliasearch';
+import { browserHistory } from 'react-router'
 import styles from '../styles/book.css';
+
 
 class Book extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      book: null
+      book: {}
     };
   }
 
-  componentDidMount() {
-    var client = algoliasearch('5XC2UZIWS0', 'cd170872e1fa42f6c6b5118e2c1f8624');
+  componentWillMount() {
+    let self = this
+    let client = algoliasearch(String(process.env.ALGOLIA_APP_ID), String(process.env.ALGOLIA_API_KEY));
     var queries = [{
-      indexName: 'Books_development',
+      indexName: String(process.env.ALGOLIA_INDEX),
       query: this.props.params.permalink,
       params: {hitsPerPage: 1},
       restrictSearchableAttributes: 'permalink'
     }];
 
     client.search(queries)
-    .then(function searchSuccess(content) {
+    .then(function(content) {
       var result = content.results[0].hits[0];
-      this.setState({
-        book: result
-      });
-      document.title = this.state.book.commonTitle;
-    }.bind(this))
-    .catch(function searchError(err) {
-      console.error(err);
-    });
+      browserHistory.push('/books/'+result.permalink);
+      document.title = result.title;
+      self.setState({book: result})
+    })
+  }
+
+  componentWillUnmount() {
+    browserHistory.push('/')
+    document.title = 'KnowThyShelf';
   }
 
   render() {
-    if (this.state.book) {
-      return (
-        <BookBox book={this.state.book} />
-      );
-    } else {
-      return(
-        <div id='loading-book'></div>
-      );
-    }
+    let book = this.state.book;
+
+    return(
+      <div>
+        <div>
+          <h1>
+            {book.title}
+          </h1>
+          <hr />
+          <div className='summary-section'>
+            <img src={book.cover_url} />
+            <div className='summary-table'>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <b>Title:</b> 
+                    </td>
+                    <td>
+                      {book.title}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Author:</b> 
+                    </td>
+                    <td>
+                      <a href={"/bookshelf?q=" + book.author}>{book.author}</a>  
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Publisher:</b> 
+                    </td>
+                    <td>
+                      <a href={"/bookshelf?q=" + book.publisher}>{book.publisher}</a> 
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Location:</b> 
+                    </td>
+                    <td>
+                      {book.city}, {book.country}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Publish Date:</b> 
+                    </td>
+                    <td>
+                      {book.publish_date}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Language:</b> 
+                    </td>
+                    <td>
+                      <a href={"/bookshelf?q=" + book.language}>{book.language}</a> 
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Genre:</b> 
+                    </td>
+                    <td>
+                      <a href={"/bookshelf?q=" + book.genre}>{book.genre}</a> 
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Pages:</b> 
+                    </td>
+                    <td>
+                      {book.pages}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Print Run:</b> 
+                    </td>
+                    <td>
+                      {book.printRun}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+    );
   }
-};
+}
+
 
 export default Book;
--- app/components/BookCover.jsx
+++ app/components/BookCover.jsx
@@ -25,7 +25,7 @@ class BookCover extends React.Component {
             <img id={book.permalink} className='book-cover' src={book.cover_url} />
           </div>
           <div className='result-details'>
-            <a href={'/book/' + book.permalink}>{book.title}</a>
+            <a href={'/books/' + book.permalink}>{book.title}</a>
             <h5>by {book.author_first} {book.author}</h5>
           </div>
         </div>
